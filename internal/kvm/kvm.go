6e7b14b976697d73e351fdc125fee61915fa61ae
Karl Redgate <kredgate@bostondynamics.com>
2025-12-05 15:24:03 -0500

    internals

---
+++ internal/kvm/kvm.go
@@ -0,0 +1,21 @@
+
+package main
+
+const (
+    KVMIO = 0xAE00
+    KVM_GET_API_VERSION = KVMIO + 0x00
+    KVM_CREATE_VM       = KVMIO + 0x01
+)
+
+func ioctl( fd uintptr, operation uintptr, arg uintptr ) (uintptr,error) {
+    result,_,errno := syscall.Syscall( syscall.SYS_IOCTL, fd, operation, arg )
+    if errno != 0 {
+	return result,errno
+    }
+    return result,nil
+}
+
+func GetAPIVersion( fd uintptr ) (uintptr,error) {
+    v := ioctl( fd, uintptr(KVM_GET_API_VERSION), uintptr(0) )
+    return v
+}
---
+++ internal/machine/machine.go
@@ -0,0 +1,9 @@
+packae machine
+
+type Machine struct {
+	Name string
+}
+
+func New() (*Machine, error) {
+	return nil, nil
+}
